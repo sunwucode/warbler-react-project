01e161849fba33de40af7949177f9270692e0063
sunwucode <wukongkhan8@gmail.com>
2019-08-21 18:25:39 +0200

    first part and auth done

--- warbler-server/index.js
+++ warbler-server/index.js
@@ -1,15 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const errorHandler = require("./handlers/error");
+const authRoutes = require("./routes/auth");
 
 const PORT = 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-
+app.use("/api/auth", authRoutes);
 //ALL my routes here !!!//
 
 app.use(function (req, res, next) {
