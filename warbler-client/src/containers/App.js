e87621ae9efc37c8dfbeae16a25a01d4c5e37bbb
sunwucode <wukongkhan8@gmail.com>
2019-08-23 12:23:10 +0200

    styling navbar

--- warbler-client/src/containers/App.js
+++ warbler-client/src/containers/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from "react";
 import {Provider} from "react-redux";
 import {configureStore} from "../store";
 import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./Navbar";
 
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
     <Router>
-      <div>Hello world</div>
+      <div className="onboarding">
+        <Navbar/>
+        </div>
     </Router>
   </Provider>
 );
